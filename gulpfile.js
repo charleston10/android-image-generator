18194b949a6a08d2cb42b4bb48c719e8084f65db
Armenio Maurício Ferreira Neto <dehme.armenio@gmail.com>
2018-10-15 12:04:26 -0300

    resize img

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
 var gulp = require('gulp');
+const flatMap = require('flat-map').default
+const scaleImages = require('gulp-scale-images')
+
+const twoVariantsPerFile = (file, cb) => {
+    const pngFile = file.clone()
+    pngFile.scale = { maxWidth: 160, maxHeight: 90, format: 'png' }
+    cb(null, [pngFile])
+}
 
 gulp.task('default', function () {
-    // place code for your default task here
-    console.log("ola")
+    gulp.src('foo_images/*.{jpeg,jpg,png,gif}')
+        .pipe(flatMap(twoVariantsPerFile))
+        .pipe(scaleImages())
+        .pipe(gulp.dest('drawable-hdpi/'));
 });
+
+/*---size img atual----1280 x 720
+
+1.5*1280/240 width
+1.5*720/240 heigth
+
+
+hdpi                240dpi 
+scaling factor      1.5px/dp
+convert to pixels   1dp * 1.5px/dp = 1.5px
+physical size       1.5px/240dpi = 1/160in
+*/
