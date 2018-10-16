935a277b98b53ae6c503921792bbeffaea812c0a
Armenio Maurício Ferreira Neto <dehme.armenio@gmail.com>
2018-10-16 06:47:53 -0300

    name + const size

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,28 @@ var gulp = require('gulp');
 const flatMap = require('flat-map').default
 const scaleImages = require('gulp-scale-images')
 
+const path = require('path')
+
+const pngWidth = 284
+const pngHeight = 160
+
 const twoVariantsPerFile = (file, cb) => {
     const pngFile = file.clone()
-    pngFile.scale = { maxWidth: 160, maxHeight: 90, format: 'png' }
+    pngFile.scale = { maxWidth: pngWidth, maxHeight: pngHeight, format: 'png' }
     cb(null, [pngFile])
+}
+
+const computeFileName = (output, scale, cb) => {
+    const fileName = [
+        path.basename(output.path, output.extname), // strip extension
+    ].join('.')
+    cb(null, fileName)
 }
 
 gulp.task('default', function () {
     gulp.src('foo_images/*.{jpeg,jpg,png,gif}')
         .pipe(flatMap(twoVariantsPerFile))
-        .pipe(scaleImages())
+        .pipe(scaleImages(computeFileName))
         .pipe(gulp.dest('drawable-hdpi/'));
 });
 
